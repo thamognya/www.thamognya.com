2aac0f2e129d64ef429e6910901eeb365f8c5332
Thamognya Kodi <contact@thamognya.com>
2022-06-17 07:13:15 +0700

    update

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,40 @@
+#![recursion_limit = "640"]
+use wasm_bindgen::prelude::*;
 use yew::prelude::*;
 
-#[function_component(App)]
-fn app() -> Html {
-    html! {
-        <h1>{ "Test" }</h1>
+mod app_router;
+mod routes;
+
+use app_router::AppRouter;
+
+pub struct Model;
+
+impl Component for Model {
+    type Message = ();
+    type Properties = ();
+    fn create(_: Self::Properties, _: ComponentLink<Self>) -> Self {
+        Self {}
+    }
+
+    fn update(&mut self, _msg: Self::Message) -> ShouldRender {
+        false
+    }
+
+    fn change(&mut self, _props: Self::Properties) -> ShouldRender {
+        // Should only return "true" if new properties are different to
+        // previously received properties.
+        // This component has no properties so we will always return "false".
+        false
+    }
+
+    fn view(&self) -> Html {
+        html! {
+            <AppRouter/>
+        }
     }
 }
 
-fn main() {
-    yew::start_app::<App>();
+#[wasm_bindgen(start)]
+pub fn run_app() {
+    App::<Model>::new().mount_to_body();
 }
