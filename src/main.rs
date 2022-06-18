bf6a787fcf81cd24e3a8facc8b6af49b6364e99a
Thamognya Kodi <contact@thamognya.com>
2022-06-18 09:14:20 +0700

    got routing to work

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,21 @@
-fn main() {
-    println!("Hello, world!");
+// imports
+use yew::prelude::*;
+// file imports
+mod router;
+
+#[function_component(App)]
+fn app() -> Html 
+{
+    html!
+    {
+        <>
+            <h1>{ "Hello" }</h1> //navbar
+            <router::Router />
+        </>
+    }
+}
+
+fn main() 
+{
+    yew::start_app::<App>();
 }
---
+++ src/router.rs
@@ -0,0 +1,105 @@
+// imports
+use yew::prelude::*;
+use yew_router::prelude::*;
+// file imports
+
+// routes that I want to use
+# [derive(Clone, Routable, PartialEq)]
+enum Route 
+{
+    #[at("/")]
+    Index,
+    #[at("/contact")]
+    Contact,
+    #[at("/projects")]
+    ProjectHomePage,
+    #[at("/projects/:id")]
+    Projects {id: u8},
+    #[at("/experience")]
+    ExperienceHomePage,
+    #[at("/experience/:id")]
+    Experience {id: u8},
+    #[at("/awards")]
+    AwardsHomePage,
+    #[at("/awards/:id")]
+    Awards {id: u8},
+    #[not_found]
+    #[at("/404")]
+    NotFound,
+}
+
+fn switch(route: &Route) -> Html
+{
+    match route
+    {
+        Route::Index => html!
+        {
+            <>
+                <h1>{ "Hello" }</h1>
+            </>
+        },
+        Route::Contact => html!
+        {
+            <>
+                <h1>{ "Contact" }</h1>
+            </>
+        },
+        Route::ProjectHomePage => html!
+        {
+            <>
+                <h1>{ "Projects List" }</h1>
+            </>
+        },
+        Route::Projects {id} => html!
+        {
+            <>
+                <p>{ format!("Projects {}", id) }</p>
+            </>
+        },
+        Route::ExperienceHomePage => html!
+        {
+            <>
+                <h1>{ "Experience List" }</h1>
+            </>
+        },
+        Route::Experience { id } => html!
+        {
+            <>
+                <p>{ format!("Experience {}", id) }</p>
+            </>
+        },
+        Route::AwardsHomePage=> html!
+        {
+            <>
+                <h1>{ "Awards List" }</h1>
+            </>
+        },
+        Route::Awards { id } => html!
+        {
+            <>
+                <p>{ format!("Awards {}", id) }</p>
+            </>
+        },
+        Route::NotFound => html!
+        {
+            <>
+                <p>{ "404 Error: Link Not Found" }</p>
+            </>
+        },
+        _ => html!
+        {
+            <>
+                <h1>{ "Hello" }</h1>
+            </>
+        }
+    }
+}
+
+#[function_component(Router)]
+pub fn router() -> Html {
+    html! {
+        <BrowserRouter>
+            <Switch<Route> render={Switch::render(switch)} />
+        </BrowserRouter>
+    }
+}
