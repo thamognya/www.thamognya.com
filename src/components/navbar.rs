6f131e310bb1e9abd517021319e1fd95cf69c65e
Thamognya Kodi <contact@thamognya.com>
2022-06-19 19:02:47 +0700

    done with navbar more or less

--- src/components/navbar.rs
+++ src/components/navbar.rs
@@ -6,12 +6,22 @@ pub fn navbar() -> Html
     html! 
     {
         <>
-            <h1>{ "NavBar" }</h1>
-            <nav>
-                <div class={ "container mx-auto" }>
-                    <h1>{ "NavBar" }</h1>
-                </div>
-            </nav>
+            <div class={ "container mx-auto max-w-3xl p-8" }> // think about py vs p
+                <nav class={ "flex" }>
+                    <div class={ "flex-none w-14" }>
+                        <img class={ "hover:rotate-45" } src={ "../assets/logo.png" } alt={ "Logo" } /> // fix this later
+                    </div>
+                    <div class={ "flex-auto w-64" }>
+                        <div class={ "flex space-x-4 justify-end" }>
+                            <h1>{ "Yew" }</h1>
+                            <h1>{ "Yew" }</h1>
+                            <h1>{ "Yew" }</h1>
+                            <h1>{ "Yew" }</h1>
+                            <h1>{ "Yew" }</h1>
+                        </div>
+                    </div>
+                </nav>
+            </div>
         </>
     }
 }
