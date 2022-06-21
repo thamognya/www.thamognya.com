9f77e73a3b36d562f092f26c31b99757a1529d95
Thamognya Kodi <contact@thamognya.com>
2022-06-21 20:42:48 +0700

    update

--- components/footer.tsx
+++ components/footer.tsx
@@ -1,15 +1,18 @@
 import { 
     Box, 
     Divider,
+    Center,
 } from '@chakra-ui/react'
 
 const Footer = () => {
     return (
-        <Box align="center" opacity={0.4} fontSize="sm" pt={5}>
-            <Divider />
-            &copy; {new Date().getFullYear()} Thamognya Kodi. All Rights
-            Reserved.
-        </Box>
+        <Center>
+            <Box opacity={0.4} fontSize="sm" pt={5}>
+                <Divider />
+                &copy; {new Date().getFullYear()} Thamognya Kodi. All Rights
+                Reserved.
+            </Box>
+        </Center>
     )
 }
 
