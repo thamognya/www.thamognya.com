3a13cdb8ac43dc95854702b8dccf258ce153103d
Thamognya Kodi <contact@thamognya.com>
2022-06-21 14:31:15 +0700

    start over with nextjs

---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+
+const Home: NextPage = () => {
+    return (
+        <>Hello</>
+    )
+}
+
+export default Home
