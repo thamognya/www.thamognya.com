c0821bd958ea5a4b8ef9b0b0b2364c2216ff8072
Thamognya Kodi <contact@thamognya.com>
2022-07-05 09:09:22 +0700

    change navbar.tsx to jsx

--- components/navbar.jsx
+++ components/navbar.jsx
@@ -18,7 +18,7 @@ import { HamburgerIcon } from '@chakra-ui/icons'
 import ThemeToggleButton from './theme-toggle-button'
 import { IoLogoGithub } from 'react-icons/io5'
 
-const LinkItem = ({ href, path, target, children, ...props }: any) => {
+const LinkItem = ({ href, path, target, children, ...props }) => {
     const active = path === href
     const inactiveColor = useColorModeValue('#525252', '#F5F5F5')
     return (
@@ -42,7 +42,7 @@ const LinkItem = ({ href, path, target, children, ...props }: any) => {
     )
 }
 
-const Navbar = ({props}: any) => {
+const Navbar = props => {
     const { path } = props
 
     return (
