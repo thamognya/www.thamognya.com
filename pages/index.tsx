66fc9ec3045f9d830f9a7770d8f0334d05dfc638
Thamognya Kodi <contact@thamognya.com>
2022-07-05 10:30:39 +0700

    update

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import type { NextPage } from 'next'
+import NextLink from 'next/link'
 import Head from 'next/head'
 import Image from 'next/image'
 import {
@@ -11,11 +12,44 @@ import {
     List,
     ListItem,
     useColorModeValue,
+    Section,
     chakra
 } from '@chakra-ui/react'
+import { ChevronRightIcon } from '@chakra-ui/icons'
+import Paragraph from '../components/paragraph'
 
-const Home: NextPage = () => {
-    return <Box>Hey</Box>
+const Home = () => {
+    return (
+      <Section delay={0.1}>
+        <Heading as="h3" variant="section-title">
+          Work
+        </Heading>
+        <Paragraph>
+          Takuya is a freelance and a full-stack developer based in Osaka with a
+          passion for building digital services/stuff he wants. He has a knack
+          for all things launching products, from planning and designing all the
+          way to solving real-life problems with code. When not online, he loves
+          hanging out with his camera. Currently, he is living off of his own
+          product called{' '}
+          <NextLink href="/works/inkdrop" passHref scroll={false}>
+            <Link>Inkdrop</Link>
+          </NextLink>
+          . He publishes content for marketing his products and his YouTube
+          channel called &quot;
+          <NextLink href="https://www.youtube.com/devaslife" passHref>
+            <Link target="_blank">Dev as Life</Link>
+          </NextLink>
+          &quot; has more than 100k subscribers.
+        </Paragraph>
+        <Box align="center" my={4}>
+          <NextLink href="/works" passHref scroll={false}>
+            <Button rightIcon={<ChevronRightIcon />} colorScheme="teal">
+              My portfolio
+            </Button>
+          </NextLink>
+        </Box>
+      </Section>
+    )
 }
 
 export default Home
