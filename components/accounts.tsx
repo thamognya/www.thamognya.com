a3b90e9bf5665a19f739ed61daaea470d4d732f3
Thamognya Kodi <contact@thamognya.com>
2022-07-08 14:42:07 +0700

    update

--- components/accounts.tsx
+++ components/accounts.tsx
@@ -9,11 +9,11 @@ const About: React.FC = () => {
                 <div className="container mx-auto px-11">
                     <p className="leading-tight max-w-5xl mx-auto text-4xl lg:text-4xl text-bg-fg">
                         <strong>Hey, my name is Thamognya Kodi. </strong>I am a
-                        HighSchool student who likes Operating System, kernels,
-                        and Machine Learning. Currently I am an proxy maintainer
-                        and contributor at Gentoo. Additionally I also am an tor
+                        high school student who likes operating systems, kernels,
+                        and machine learning. Currently, I am a developer
+                        and contributor at Gentoo. Additionally, I am a Tor
                         relay provider and mirror provider to other
-                        distrobutions
+                        Linux and BSD distros.
                     </p>
                 </div>
                 <div className="container mx-auto px-11 bg-bg-bg text-center mt-28 text-bg-fg">
