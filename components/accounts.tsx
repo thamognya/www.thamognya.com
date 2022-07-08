339d21c31da639c3ed229cf7223a1420539a4fee
Thamognya Kodi <contact@thamognya.com>
2022-07-08 11:47:18 +0700

    white space on both phone nad comp removed

--- components/accounts.tsx
+++ components/accounts.tsx
@@ -5,7 +5,7 @@ import Social from './socialMedia'
 const About: React.FC = () => {
     return (
         <>
-            <section className="flex flex-col bg-bg-bg py-20 text-3xl md:text-4 h-screen">
+            <section className="flex flex-col bg-bg-bg py-20 text-3xl md:text-4 h-screen md:h-screen">
                 <div className="container mx-auto px-11">
                     <p className="leading-tight max-w-5xl mx-auto text-4xl lg:text-4xl text-bg-fg">
                         <strong>Hey, my name is Thamognya Kodi. </strong>I am a
@@ -16,10 +16,10 @@ const About: React.FC = () => {
                         distrobutions
                     </p>
                 </div>
-                <div className="container mx-auto px-11 text-center mt-28 text-bg-fg">
-                    <h2 className="font-bold">Social Media</h2>
-                    <div className="mt-10">
-                        <Social />
+                <div className="container mx-auto px-11 bg-bg-bg text-center mt-28 text-bg-fg">
+                        <h2 className="font-bold">Social Media</h2>
+                        <div className=" mt-10">
+                            <Social />
                     </div>
                 </div>
             </section>
