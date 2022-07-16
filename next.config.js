8c79a42e03069dbf8af510d62f16c7c24bb203ff
Thamognya Kodi <contact@thamognya.com>
2022-07-16 09:18:08 +0700

    edge server rendering enabled

--- next.config.js
+++ next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true
+    reactStrictMode: true,
+    experimental: {
+        runtime: 'experimental-edge',
+    },
 }
 
 module.exports = nextConfig
