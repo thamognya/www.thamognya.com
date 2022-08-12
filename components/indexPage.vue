1174947d0c064f1096350918f59a2f7f68ba01a0
Thamognya Kodi <contact@thamognya.com>
2022-08-12 19:29:10 +0700

    fix(weebsitee): update

---
+++ components/indexPage.vue
@@ -0,0 +1,11 @@
+<template>
+    <h1>h</h1>
+</template>
+
+<script lang="ts">
+import Vue from 'vue'
+
+export default Vue.extend({
+    name: 'IndexPage'
+})
+</script>
--- pages/index.vue
+++ pages/index.vue
@@ -1,11 +1,16 @@
 <template>
-  <h1 class="text-3xl">hello</h1>
+    <div class="font-fira">
+        <IndexPage />
+        <h1>jdf</h1>
+    </div>
 </template>
 
 <script lang="ts">
 import Vue from 'vue'
+import IndexPage from '../components/indexPage.vue'
 
 export default Vue.extend({
-  name: 'IndexPage',
+    name: 'IndexPage',
+    components: { IndexPage }
 })
 </script>
