97e860e4d444acad8ab55659df9bf308817d2815
Thamognya Kodi <contact@thamognya.com>
2022-10-11 10:31:13 +0700

    new cool api

--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -3,11 +3,15 @@ import type { NextApiRequest, NextApiResponse } from 'next'
 
 type Data = {
     name: string
+    github: string
 }
 
 export default function handler(
     req: NextApiRequest,
     res: NextApiResponse<Data>
 ) {
-    res.status(200).json({ name: 'John Doe' })
+    res.status(200).json({
+        name: 'Thamognya',
+        github: 'https://github.com/thamognya'
+    })
 }
