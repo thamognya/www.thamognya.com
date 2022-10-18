e683b992cb7cbeb3d5479d60e8443f44e46e2a3b
Thamognya Kodi <contact@thamognya.com>
2022-10-18 19:33:45 +0700

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -46,37 +46,38 @@ import image from "@astrojs/image";
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
+import cloudflare from "@astrojs/cloudflare";
+
+// https://astro.build/config
+import deno from "@astrojs/deno";
+
+// https://astro.build/config
+import netlify from "@astrojs/netlify/functions";
+
+// https://astro.build/config
+import node from "@astrojs/node";
+
+// https://astro.build/config
+
+// https://astro.build/config
 export default defineConfig({
-    site: 'https://www.thamognya.com',
-    integrations: [
-        react(),
-        vue(),
-        svelte(),
-        solidJs(),
-        preact(),
-        lit(),
-        alpinejs(),
-        tailwind({
-            config: {
-                path: './tailwind.config.cjs'
-            },
-            config: {
-                applyBaseStyles: false
-            }
-        }),
-        mdx(),
-        compress(),
-        sitemap(),
-        prefetch({
-            // Just add rel="prefetch" to any <a />
-            throttle: 3
-        }),
-        image(),
-        partytown({
-            // Partytown should be ready to go with zero config. If you have an existing 3rd party script on your site, try adding the type="text/partytown" attribute:
-            config: { debug: false }
-        })
-    ],
-//    output: 'server',
-//    adapter: vercel()
-})
+  site: 'https://www.thamognya.com',
+  integrations: [react(), vue(), svelte(), solidJs(), preact(), lit(), alpinejs(), tailwind({
+    config: {
+      path: './tailwind.config.cjs'
+    },
+    config: {
+      applyBaseStyles: false
+    }
+  }), mdx(), compress(), sitemap(), prefetch({
+    // Just add rel="prefetch" to any <a />
+    throttle: 3
+  }), image(), partytown({
+    // Partytown should be ready to go with zero config. If you have an existing 3rd party script on your site, try adding the type="text/partytown" attribute:
+    config: {
+      debug: false
+    }
+  })],
+  //    output: 'server',
+  //    adapter: vercel()
+});
